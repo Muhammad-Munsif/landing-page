d4f8b8d5f392d759c652ed9a532038c5b874f455
MUHAMMAD-MUNSIF <m.munsif4375@gmail.com>
2025-04-06 23:33:16 +0500

    I have done my landing page.

---
+++ script.js
@@ -0,0 +1,6 @@
+// Example: Show alert when contact form is submitted
+document.querySelector("form").addEventListener("submit", function (e) {
+    e.preventDefault();
+    alert("Message sent! We'll get back to you soon.");
+  });
+  
