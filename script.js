16c0a2cd2c1eaac3ff7d8ea6fe8356b31290f7d3
Muhammad-Munsif <m.munsif4375@gmail.com>
2025-09-18 19:35:26 +0500

    small tasked added to JS file

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 // Example: Show alert when contact form is submitted
 document.querySelector("form").addEventListener("submit", function (e) {
-    e.preventDefault();
-    alert("Message sent! We'll get back to you soon.");
-  });
-  
+  e.preventDefault();
+  alert("Message sent! We'll get back to you soon.");
+});
